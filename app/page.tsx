020b261a04d8cc671df038f2321670c44c3931b6
Frank-Vez <vezina.francis.04@gmail.com>
2025-01-07 23:15:27 -0500

    fixed a lint error + remmoved no-unescaped-entities from lint rulesjs

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ export default function Home() {
   return (
     <div className="container mx-auto px-4 py-2 justify-items-center">
       <h1 className={`${lato.className} py-2`}>Welcome to your Neighborhood</h1>
+      <p className={`${lusitana.className} py-2`}>
+        start by creating an account and in no time you'll feel more connected
+        to the people around you.
+      </p>
       <ArrowDownCircleIcon className="size-10 fill-black" />
     </div>
   );
